e8cd86c27d47427b517960b1050af08fcc077c8b
Mriyam Tamuli <mbtamuli@gmail.com>
2021-05-30 17:23:57 +0530

    :sparkles: NEW: Add basic telegram bot
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

---
+++ cmd/bot.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+const shutdownTimeout = 60
+
+func main() {
+	go func() {
+		log.Println("Starting listener")
+		time.Sleep(shutdownTimeout * time.Second)
+		log.Printf("Exiting listener after %d seconds\n", shutdownTimeout)
+		os.Exit(0)
+	}()
+
+	botAPIToken, err := getAPIToken("TELEGRAM_BOT_TOKEN")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bot, err := tgbotapi.NewBotAPI(botAPIToken)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	if err != nil {
+		log.Printf("%s\n", err.Error())
+	}
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
+}
+
+func getAPIToken(key string) (string, error) {
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		return "", fmt.Errorf("environment variable not set")
+	}
+	return val, nil
+}
